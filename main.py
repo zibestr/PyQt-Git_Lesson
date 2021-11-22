44ec7a7421aa23cfd14be04da90b1d9bd0eb7f21
zibestr <danila.yashin23@gmail.com>
2021-11-22 20:45:12 +0300

    Add UI.py file, edit a core app

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import sys
 
-from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QColor, QPen
 from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5 import uic
+from UI import Ui_Form
 
 from random import randint
 
 
-class CirclesApp(QWidget):
+class CirclesApp(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.isDraw = False
         self.pushButton.clicked.connect(self.draw_circles)
 
@@ -24,14 +23,21 @@ class CirclesApp(QWidget):
         if self.isDraw:
             qp.begin(self)
 
-            qp.setPen(QPen(Qt.yellow, 8))
-
             d1 = randint(100, 400)
             d2 = randint(100, 400)
             d3 = randint(100, 400)
 
+            qp.setPen(QPen(QColor(randint(1, 255),
+                                  randint(1, 255),
+                                  randint(1, 255)), 8))
             qp.drawEllipse(75, 40, d1 // 2, d1 // 2)
+            qp.setPen(QPen(QColor(randint(1, 255),
+                                  randint(1, 255),
+                                  randint(1, 255)), 8))
             qp.drawEllipse(10, 70, d2 // 2, d2 // 2)
+            qp.setPen(QPen(QColor(randint(1, 255),
+                                  randint(1, 255),
+                                  randint(1, 255)), 8))
             qp.drawEllipse(80, 50, d3 // 2, d3 // 2)
 
             qp.end()
